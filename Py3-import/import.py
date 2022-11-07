e5e249a241d997937d89182c98bfdd6e31bbc20c
devSATURNO <110566544+devSATURNO@users.noreply.github.com>
2022-11-07 19:51:51 +0100

    ok

---
+++ Py3-import/import.py
@@ -0,0 +1,12 @@
+#from math import sqrt, ceil
+
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+
+#print(f'A raiz de {num} é {ceil(raiz)}')
+
+#import random
+#num = random.randint(1,10)
+#print(num)
+
+
---
+++ Py3-import/test.py
@@ -0,0 +1,7 @@
+import math
+
+num = float(input('Digite um número real: '))
+
+pinteira = math.floor(num)
+
+print(f'A parte inteira de {num} é {pinteira}')
